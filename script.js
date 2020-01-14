90cd404d6e1d85a3517af2d16a4355ddbf7e6490
Shangni <shangnihu@gmail.com>
2020-01-13 23:29:18 -0500

    Finished project page, changed linking to work the way client wants it to on all pages, added about page.

--- script.js
+++ script.js
@@ -5,8 +5,14 @@ $(function () {
   $('.top-nav ul #page').css('border-bottom-color', '#ffa85c');
 
   $('nav li').click((e) => {
-    $('nav li a').css('border-bottom-color', 'transparent')
-    $(e.target).css('border-bottom-color', '#ffa85c');
+
+    if (e.target.text != "projects") {
+
+      $('nav li a').css('border-bottom-color', 'transparent')
+      $(e.target).css('border-bottom-color', '#ffa85c');
+    } else {
+      $(e.target).css('border-bottom-color', 'transparent');
+    }
 
   })
 });
